9548d74965439a13899890d8186f90ca74a46090
kaushik <kaushik.varanasi1@gmail.com>
2017-03-23 14:40:03 +0530

    displaying only request body and url

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(details)
     {
     	if(details.method == "POST"){
-            bkg.console.log(JSON.stringify(details));
+            bkg.console.log(JSON.stringify(details.requestBody), JSON.stringify(details.url));
     	}
     },
     {urls: [ "<all_urls>" ]},
